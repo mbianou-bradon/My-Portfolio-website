a584320b97fb8db29438ac95141ea9fc49df36d4
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-08 12:49:36 +0100

    Created Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from './components/Footer';
+import Hero from './components/Hero';
 import NavBar from './components/Navbar';
 
 function App() {
   return (
     <div className="bg-primary text-white font-mono">
       <NavBar />
+      <Hero/>
       <Footer/>
     </div>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+
+
+export default function Home(){
+
+    return (
+        <div>
+            <Hero/>
+        </div>
+    )
+}
