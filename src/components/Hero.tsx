f9102ded8cbd5a7a862e2a81a6a609bdce835bfc
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-10 11:43:38 +0100

    Made Hero section responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,22 +4,22 @@ import bradon_hero from "../assets/images/bradon_hero.png"
 export default function Hero(){
 
     return (
-        <div className="h-[80vh] bg-red-60">
-            <div className="grid grid-cols-2 h-full">
-                <div className="h-[70%] w-[80%] my-auto mx-auto">
+        <div className="sm:h-[80vh] mb-[6.25rem]">
+            <div className="grid grid-cols-1 sm:grid-cols-2 h-full">
+                <div className="h-[85%] sm:h-[70%] w-[90%] sm:w-[80%] my-auto mx-auto">
                     <div className="p-2 rounded bg-secondary text-slate-900 text-xs w-fit"><h2>Full Stack Developer</h2></div>
                     <div className="mt-7 mb-[4.5rem]">
-                        <h2 className="text-5xl mb-5 leading-[3rem]">
+                        <h2 className="text-4xl sm:text-5xl mb-5 leading-[3rem]">
                             Talk is Cheap. Show me the code
                         </h2>
-                        <p className="text-xs text-tertiary w-[75%] leading-[1.5rem]">I design and code beautifully simple things, and I am passionate about UI.</p>
+                        <p className="text-xs text-tertiary w-[85%] sm:w-[75%] leading-[1.5rem]">I design and code beautifully simple things, and I am passionate about UI.</p>
                     </div>
 
                     <div className="text-xl uppercase text-secondary underline cursor-pointer hover:no-underline font-semibold">
                         <a href="#">Let's Chat!</a>
                     </div>
                 </div>
-                <div className="w-full h-full flex items-end">
+                <div className="hidden w-full h-full sm:flex items-end">
                     <img src={bradon_hero} alt="Bradon Profile" title="Bradon Profile" className="h-[80%] w-[80%] aspect-square object-center"/>
                 </div>
             </div>
