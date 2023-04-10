9e058d988413d5fb815a1ea637209d9fff88d527
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-10 11:38:36 +0100

    Hero section template completed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,27 @@
-
+import React from "react"
+import bradon_hero from "../assets/images/bradon_hero.png"
 
 export default function Hero(){
 
     return (
-        <div className="h-[80vh]">
+        <div className="h-[80vh] bg-red-60">
             <div className="grid grid-cols-2 h-full">
-                <div className="h-[70%] w-[70%] my-auto mx-auto">
+                <div className="h-[70%] w-[80%] my-auto mx-auto">
                     <div className="p-2 rounded bg-secondary text-slate-900 text-xs w-fit"><h2>Full Stack Developer</h2></div>
-                    <div className="mt-4 mb-[4.5rem]">
-                        <h2 className="text-4xl w-[80%] mb-5 leading-[3rem]">
+                    <div className="mt-7 mb-[4.5rem]">
+                        <h2 className="text-5xl mb-5 leading-[3rem]">
                             Talk is Cheap. Show me the code
                         </h2>
-                        <p className="text-xs text-tertiary w-[75%] leading-[1rem]">I design and code beautifully simple things, and I am passionate about UI.</p>
+                        <p className="text-xs text-tertiary w-[75%] leading-[1.5rem]">I design and code beautifully simple things, and I am passionate about UI.</p>
                     </div>
 
-                    <div className="uppercase text-secondary underline cursor-pointer hover:no-underline font-semibold">
+                    <div className="text-xl uppercase text-secondary underline cursor-pointer hover:no-underline font-semibold">
                         <a href="#">Let's Chat!</a>
                     </div>
                 </div>
-                <div></div>
+                <div className="w-full h-full flex items-end">
+                    <img src={bradon_hero} alt="Bradon Profile" title="Bradon Profile" className="h-[80%] w-[80%] aspect-square object-center"/>
+                </div>
             </div>
         </div>
     )
