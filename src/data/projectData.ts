d53d4db4a7fb7f7fcad68bc537ab2e4d654d8bb4
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-19 22:57:49 +0100

    Created Data for Project

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,3 +1,115 @@
 export const ProjectData = [
+    {
+        title: "Adobe2.0",
+        desc: "This project involved the recreation the landing page of the official website of adobe.com using pure and basic technologies. NO External Frameworks",
+        images: [],
+        techStacks: ["HTML5","CSS"],
+        socialLinks: {
+            liveLink: "https://adobe-homepage-clone.netlify.app/",
+            GitHub: "https://github.com/mbianou-bradon/Adobe2.0"
+        },
+        category: ["FRONTEND"],
+    },
+    {
+        title: "BradFitness - SIO Landing Page",
+        desc: "This project involved reproducing the SIO landing page create on dribble.",
+        images: [],
+        techStacks: ["HTML5", "CSS"],
+        socialLinks: {
+            liveLink: "https://brad-fitness.netlify.app/",
+            GitHub: "https://github.com/mbianou-bradon/iknite-exercise/tree/main/BradFitness"
+        },
+        category: ["FRONTEND"],
+    },
+    {
+        title: "BradMarketing-TW",
+        desc: "This project involved reproducing the Besnik Landing Page created on dribble. The Besnik Landing page is for a marketing site that mainly helps in the visibilty of Companies",
+        images: [],
+        techStacks: ["HTML5", "TAILWINDCSS"],
+        socialLinks: {
+            liveLink: "https://bradmarketing-tw.netlify.app/",
+            GitHub: "https://github.com/mbianou-bradon/iknite-exercise/tree/main/BradMarketing-TW",
+        },
+        category: ["FRONTEND"],
+    },
+    {
+        title: "BradMarketing",
+        desc: "This project involved reproducing the Besnik Landing Page created on dribble. The Besnik Landing page is for a marketing site that mainly helps in the visibilty of Companies",
+        images: [],
+        techStacks: ["HTML5", "BOOTSTRAP"],
+        socialLinks: {
+            liveLink: "https://bradmarketing-bootstrap.netlify.app/",
+            GitHub: "https://github.com/mbianou-bradon/iknite-exercise/tree/main/BradMarketing",
+        },
+        category: ["FRONTEND"],
+    },
+    {
+        title: "ikCalculator",
+        desc: "IkCalculator is a Basic web calculator. This calculator can perform basic operations like addition, substraction, multiplications and division.",
+        images: [],
+        techStacks: ["HTML", "TAILWINDCSS", "JAVASCRIPT"],
+        socialLinks: {
+            liveLink: "https://ikcalculator.netlify.app/",
+            GitHub: "https://github.com/mbianou-bradon/iknite-Ex5-calculator"
+        },
+        category: ["FRONTEND"]
+    },
+    {
+        title: "Bradinar",
+        desc: "Bradinar(webinar platfomr) is a software tool used for hosting many events online. The events hosted are seminars, conferences, and workshops. Bradinar applications can be downloaded and stored on a computer. It can also be broadcast straight from the web platform. Bradinar platform streams live webinars to large groups of individuals.",
+        images: [],
+        techStacks: ["REACT", "REDUX", "TYPESCRIPT"],
+        socialLinks: {
+            liveLink: "https://bradinar.netlify.app/",
+            GitHub: "https://github.com/mbianou-bradon/Bradinar"
+        },
+        category: ["FRONTEND", "BACKEND"]
+    },
+    {
+        title: "IkniteSpaceIn",
+        desc: "IkniteSpaceIn is a platform where you get to know more about the Talents and Projects of Iknite Space. This is the LinkedIn of Iknite Space",
+        images: [],
+        techStacks: ["REACT", "REDUX", "TYPESCRIPT", "FIREBASE"],
+        socialLinks: {
+            liveLink: "https://spacein.vercel.app/",
+            GitHub: "https://github.com/mbianou-bradon/iknite-portforlio-management-system"
+        },
+        category: ["FRONTEND"]
+    },
+    {
+        title: "academia",
+        desc: "Academia is an online school voting platform that simplifies your yearly school elections ranging from departmental to faculty. Departmental, Faculty and School student board elections has never been this easy",
+        images: [],
+        techStacks: ["REACT", "REDUX", "JAVASCRIPT", "FIREBASE"],
+        socialLinks: {
+            liveLink: "https://academia-delta.vercel.app/",
+            GitHub: "https://github.com/mbianou-bradon/academic-voting-app"
+        },
+        category: ["FRONTEND", "BACKEND"]
+    },
+    {
+        title: "Fitness Space",
+        desc: "Fitness Space is the mobile application platform that helps you get the body shape and structure you have always dreamt about.",
+        images: [],
+        techStacks: ["REACTNATIVE", "REDUX", "TYPESCRIPT"],
+        socialLinks: {
+            liveLink: "",
+            GitHub: "https://github.com/mbianou-bradon/ikniteSpace-Fitness-App"
+        },
+        category: ["MOBILE"]
+    },
+    {
+        title: "Fitness Space Backend",
+        desc: "Fitness Space App is a Mobile Application that helps you to keep you fit and healthy. It consists of different type of workouts all grouped in to different categories. Read the Docs for more info",
+        images: [],
+        techStacks: ["NODEJS", "EXPRESSJS", "TYPESCRIPT", "MONGODB"],
+        socialLinks: {
+            liveLink: "https://fitness-space.onrender.com/",
+            GitHub: "https://github.com/mbianou-bradon/fitnessapp-backend-api-application"
+        },
+        category: ["BACKEND"]
+
+    }
+
     
 ]
