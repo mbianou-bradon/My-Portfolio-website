96aa3d414c3b82ffa2f743641edda1bbfc255daf
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-04-20 12:22:58 +0100

    Updated images for each project

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -2,7 +2,13 @@ export const ProjectData = [
     {
         title: "Adobe2.0",
         desc: "This project involved the recreation the landing page of the official website of adobe.com using pure and basic technologies. NO External Frameworks",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FAdobe-1.png?alt=media&token=8dc465da-1e10-4daf-808f-c129decbecbc",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FAdobe-2.png?alt=media&token=67e5418d-f1a0-4de6-a1cb-7c6bbbd1ddaa",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FAdobe-3.png?alt=media&token=7cdf57ef-3f72-44b9-b49f-c57751a81e53",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FAdobe-4.png?alt=media&token=afe1e6b0-72f2-4a23-bc62-6f29129a3cd4",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FAdobe-5.png?alt=media&token=876d2a46-be3b-40e5-aaa0-eb66bdb4c3e1"
+        ],
         techStacks: ["HTML5","CSS"],
         socialLinks: {
             liveLink: "https://adobe-homepage-clone.netlify.app/",
@@ -13,7 +19,10 @@ export const ProjectData = [
     {
         title: "BradFitness - SIO Landing Page",
         desc: "This project involved reproducing the SIO landing page create on dribble.",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FBradFitness1.png?alt=media&token=04663e0f-691c-4608-876c-1bd986a656f1",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FBradFitness2.png?alt=media&token=696629f7-e4b5-4c75-80f3-fd3b54d0a813"
+        ],
         techStacks: ["HTML5", "CSS"],
         socialLinks: {
             liveLink: "https://brad-fitness.netlify.app/",
@@ -24,7 +33,11 @@ export const ProjectData = [
     {
         title: "BradMarketing-TW",
         desc: "This project involved reproducing the Besnik Landing Page created on dribble. The Besnik Landing page is for a marketing site that mainly helps in the visibilty of Companies",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbesknik3.png?alt=media&token=df49d53e-10e0-4849-9664-5039ab876e02",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbesknik1.png?alt=media&token=d65871d4-f33e-4f0b-977a-287c8afbb44b",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbesknik2.png?alt=media&token=52928b84-93db-4137-b351-bbca05f343d1"
+        ],
         techStacks: ["HTML5", "TAILWINDCSS"],
         socialLinks: {
             liveLink: "https://bradmarketing-tw.netlify.app/",
@@ -35,7 +48,11 @@ export const ProjectData = [
     {
         title: "BradMarketing",
         desc: "This project involved reproducing the Besnik Landing Page created on dribble. The Besnik Landing page is for a marketing site that mainly helps in the visibilty of Companies",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbesknik3.png?alt=media&token=df49d53e-10e0-4849-9664-5039ab876e02",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbesknik1.png?alt=media&token=d65871d4-f33e-4f0b-977a-287c8afbb44b",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbesknik2.png?alt=media&token=52928b84-93db-4137-b351-bbca05f343d1"
+        ],
         techStacks: ["HTML5", "BOOTSTRAP"],
         socialLinks: {
             liveLink: "https://bradmarketing-bootstrap.netlify.app/",
@@ -46,7 +63,10 @@ export const ProjectData = [
     {
         title: "ikCalculator",
         desc: "IkCalculator is a Basic web calculator. This calculator can perform basic operations like addition, substraction, multiplications and division.",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fikcalculator-1.png?alt=media&token=ca235a08-59da-4f09-9979-07be8984c86c",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fikcalculator-2.png?alt=media&token=be98b8f1-f79d-4060-886b-ae5ade917dc7"
+        ],
         techStacks: ["HTML", "TAILWINDCSS", "JAVASCRIPT"],
         socialLinks: {
             liveLink: "https://ikcalculator.netlify.app/",
@@ -57,7 +77,10 @@ export const ProjectData = [
     {
         title: "Bradinar",
         desc: "Bradinar(webinar platfomr) is a software tool used for hosting many events online. The events hosted are seminars, conferences, and workshops. Bradinar applications can be downloaded and stored on a computer. It can also be broadcast straight from the web platform. Bradinar platform streams live webinars to large groups of individuals.",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbradinar-1.png?alt=media&token=5fc79b93-d595-47ff-b3cf-41b03d4c1e7e",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Fbradinar-2.png?alt=media&token=3031622f-1dd3-48cd-bf55-4b08a242d882"
+        ],
         techStacks: ["REACT", "REDUX", "TYPESCRIPT"],
         socialLinks: {
             liveLink: "https://bradinar.netlify.app/",
@@ -68,7 +91,13 @@ export const ProjectData = [
     {
         title: "IkniteSpaceIn",
         desc: "IkniteSpaceIn is a platform where you get to know more about the Talents and Projects of Iknite Space. This is the LinkedIn of Iknite Space",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FspaceIn-1.png?alt=media&token=ba50c17d-df5e-4f0b-844a-0a8e21fa524c",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FspaceIn-2.png?alt=media&token=ae1e6c8c-df8b-4b59-8573-0dbf8418d567",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FspaceIn-3.png?alt=media&token=ba3dafb3-61d6-4416-9f17-37f22451a616",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FspaceIn-4.png?alt=media&token=502a5ebd-42bd-458f-818a-a5acbbd59d65",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2FspaceIn-5.png?alt=media&token=54bb6b2f-b5e9-459f-80e6-d9d74ee2012b"
+        ],
         techStacks: ["REACT", "REDUX", "TYPESCRIPT", "FIREBASE"],
         socialLinks: {
             liveLink: "https://spacein.vercel.app/",
@@ -79,7 +108,11 @@ export const ProjectData = [
     {
         title: "academia",
         desc: "Academia is an online school voting platform that simplifies your yearly school elections ranging from departmental to faculty. Departmental, Faculty and School student board elections has never been this easy",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Facademia-1.png?alt=media&token=875353e9-5514-4915-a57a-4a06ec26a345",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Facademia-2.png?alt=media&token=a2b92e38-7ae1-402b-a42c-82411040d590",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Facademia-3.png?alt=media&token=fb69649d-349a-412c-9001-921d10f035f4"
+        ],
         techStacks: ["REACT", "REDUX", "JAVASCRIPT", "FIREBASE"],
         socialLinks: {
             liveLink: "https://academia-delta.vercel.app/",
@@ -101,7 +134,12 @@ export const ProjectData = [
     {
         title: "Fitness Space Backend",
         desc: "Fitness Space App is a Mobile Application that helps you to keep you fit and healthy. It consists of different type of workouts all grouped in to different categories. Read the Docs for more info",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffitness-backend-1.png?alt=media&token=4794f52a-1513-4715-871b-240f7d56df58",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffitness-backend-2.png?alt=media&token=385ec1fc-7eb7-4251-8a3a-81af98dda66d",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffitness-backend-3.png?alt=media&token=2d802031-e629-4db3-bc95-19b7c116b92f",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffitness-backend-4.png?alt=media&token=87af3318-2883-45d3-87bc-d3bc3de8c595"
+        ],
         techStacks: ["NODEJS", "EXPRESSJS", "TYPESCRIPT", "MONGODB"],
         socialLinks: {
             liveLink: "https://fitness-space.onrender.com/",
@@ -113,7 +151,12 @@ export const ProjectData = [
     {
         title: "Farmino",
         desc: "An efarm platform where farmers can list and showcase their items, buyers can see, buy and read more about the items and go forward to purchase the product. Farmers can upload products, edit, delete and even modify the items uploaded",
-        images: [],
+        images: [
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffarmino1.png?alt=media&token=a390f0eb-97aa-4e40-bb27-c7ff22e22759",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffarmino2.png?alt=media&token=4b312bcf-6a01-4f4c-bb9e-30543bc81726",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffarmino3.png?alt=media&token=505d3e30-218f-42c1-b55f-51f8a145c65c",
+            "https://firebasestorage.googleapis.com/v0/b/spacein-90478.appspot.com/o/projectImages%2Ffarmino4.png?alt=media&token=3f547850-8005-4d63-9fd7-36408bdd32f1"
+        ],
         techStacks: ["HTML", "TAILWINDCSS", "JAVASCRIPT"],
         socialLinks: {
             liveLink: "https://farmino.netlify.app/",
