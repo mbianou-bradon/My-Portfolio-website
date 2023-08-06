790d0695db9c3017f2e6f545164cfb3993549561
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-08-06 19:59:19 +0100

    fix(app): Handle Toastify problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
-import { Outlet, createBrowserRouter, RouterProvider} from "react-router-dom"
-import { NavBar, Footer } from './components';
-import { Home,Blogs, About, Contact, Error, ProjectDetails, Projects } from './pages';
-
-
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NavBar, Footer } from "./components";
+import {
+  Home,
+  Blogs,
+  About,
+  Contact,
+  Error,
+  ProjectDetails,
+  Projects,
+} from "./pages";
+import { ToastContainer } from "react-toastify";
 
 const MyApp = () => {
-
-
   return (
     <div className="bg-primary text-white font-mono [&_*]:transition-transform [&_*]:ease-linear [&_*]:duration-200">
-        <NavBar/>
-          <div>
-             <Outlet />
-          </div>
-        <Footer/>
+      <NavBar />
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
+      <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-const router = createBrowserRouter (
-  [
-    {
-      element: <MyApp />,
-      errorElement: <Error />,
-      children: [ 
-        { path: '/', element: <Home />,},
-        { path: '/aboutme', element: <About />},
-        { path: '/contact', element: <Contact />},
-        { path: '/blogs', element: <Blogs />},
-        { path: '/projects', element: <Projects/>},
-        { path: '/projects/:projectId', element: <ProjectDetails/>},
-
-      ]
-    }
-  ]
-)
+const router = createBrowserRouter([
+  {
+    element: <MyApp />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/aboutme", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/blogs", element: <Blogs /> },
+      { path: "/projects", element: <Projects /> },
+      { path: "/projects/:projectId", element: <ProjectDetails /> },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="App text-slate-800 max-w-[85rem] mx-auto">
-     <RouterProvider router = {router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
